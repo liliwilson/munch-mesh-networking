103bac1e410289e82ccb31914b873307bc42f24f
Sean Cheng <seanc2841@gmail.com>
2023-12-12 14:49:51 -0500

    added tests for cope and fixed normal tests

--- test_cope/tests.py
+++ test_cope/tests.py
@@ -1,27 +1,33 @@
-from mesh.arena import Arena
-from mesh.packet import Packet
+from cope.arena import Arena
+from cope.packet import Packet
 
 
 def test_parse() -> None:
     """
     Tests parsing of a simple one-node arena
     """
-    arena = Arena("./test_mesh/test_arenas/basic.json")
+    arena = Arena("./test_cope/test_arenas/basic.json")
     assert 'n1' in arena.get_nodes(), "expected 'n1' to be part of the arena"
     return
 
 
-def test_two_nodes() -> None:
+def test_wheel_top() -> None:
     """
-    Tests a two-node network where the nodes should be linked.
+    Tests a wheel topology network where the nodes should be linked.
     """
-    arena = Arena("./test_mesh/test_arenas/two-nodes.json")
+    arena = Arena("./test_cope/test_arenas/wheet-top.json")
     node_mapping = arena.get_nodes()
-    assert 'n1' in node_mapping and 'n2' in node_mapping, "Wrong nodes in network"
+    for n in ['n1', 'n2', 'n3', 'n4', 'n5']:
+        assert n in node_mapping, "Wrong nodes in network"
 
     # tests for links
 
-    links = [('n1', 'n2')]
+    links = [
+        ('n1', 'n2'),
+        ('n1', 'n3'),
+        ('n1', 'n4'),
+        ('n1', 'n5')
+    ]
     for node1, node2 in links:
         assert arena.can_link(node1, node2), node1 + \
             ' and ' + node2 + ' should be linked'
@@ -37,11 +43,12 @@ def test_two_nodes() -> None:
     packet = n1.get_queue_state()[0]
     assert packet.get_is_request(), 'packet should be a request packet'
 
-    arena.run()  # TODO: change this to have the parameters required
+    arena.run()
 
-    # TODO: Check if this is correct (read the error message)
-    assert len(n2.get_queue_state(
-    )) == 1, 'If this check is failing it is because I am imagining n2 instantly getting a response for n1'
+    # TODO: what happens to the true receiver
+    for n in ['n2', 'n3', 'n4', 'n5']:
+        assert len(node_mapping[n].get_queue_state(
+        )) == 1, 'all nodes should have received a packet'
 
     arena.run()  # TODO: change this to have the parameters required
 
@@ -52,7 +59,7 @@ def test_generating_links() -> None:
     """
     Tests all of the partitions of whether a link will be established between two nodes.
     """
-    arena = Arena('./test_mesh/test_arenas/all-link-partitions.json')
+    arena = Arena('./test_cope/test_arenas/all-link-partitions.json')
     node_mapping = arena.get_nodes()
     for n in ['n1', 'n2', 'n3', 'n4']:
         assert n in node_mapping, 'expected ' + n + ' to be in node_mapping'
@@ -79,7 +86,7 @@ def test_shortest_path() -> None:
     """
     Tests that the routing protocol is working correctly
     """
-    arena = Arena('./test_mesh/test_arenas/dfs.json')
+    arena = Arena('./test_cope/test_arenas/dfs.json')
     node_mapping = arena.get_nodes()
     for i in range(4):
         for j in range(4):
@@ -103,16 +110,19 @@ def test_shortest_path() -> None:
                                  'n03'], 'shortest path is wrong'
 
     arena.run()
-    for n in ['n11', 'n21']:
+    for n in ['n11', 'n01', 'n10', 'n21', 'n31', 'n20']:
         assert len(node_mapping[n].get_queue_state()
                    ) == 1, n + ' should have one packet in queue after one timestep'
+
+    # TODO: I'm not entirely sure what to do with the other nodes that are not on the path
+    # but get the packet anyway ... will test that here after we discuss though
 
 
 def test_collision() -> None:
     """
     Tests collision behavior
     """
-    arena = Arena("./test_mesh/test_arenas/collision.json")
+    arena = Arena("./test_cope/test_arenas/collision.json")
     node_mapping = arena.get_nodes()
     for i in range(1, 4):
         n = 'n' + str(i)
@@ -127,20 +137,22 @@ def test_collision() -> None:
     n3_first = n3.get_queue_state()[0]
     arena.run()  # should have a collision, so no queues are changed
 
-    if len(n1.get_queue_state()) == 2:
+    if len(n1.get_queue_state()) == 1:
+        assert len(n3.get_queue_state()
+                   ) == 2, 'if n1 sent, then n3 should not have sent'
+        assert n3.get_queue_state(
+        )[0] == n3_first, 'makes sure the same packet is at n3\'s head'
+    elif len(n3.get_queue_state()) == 0:
+        assert len(n1.get_queue_state()
+                   ) == 3, 'n1 should have not sent, and received a packet'
         assert n1.get_queue_state()[
             0] == n1_first, 'expected to the same packet to be at the head of n1\'s queue'
-        assert len(n3.get_queue_state()
-                   ) == 0, 'if n1 didn\'t send, then n3 should have sent'
-    elif len(n3.get_queue_state()) == 1:
-        assert n3.get_queue_state()[
-            0] == n3_first, 'expected to the same packet to be at the head of n3\'s queue'
-        assert len(n1.get_queue_state()) == 1, 'n1 should have sent a packet'
     else:
         raise AssertionError('neither queue sent anything')
 
     n2 = node_mapping['n2']
     assert len(n2.get_queue_state()) == 1, 'n2 should have gotten a packet'
+
     return
 
 
@@ -148,7 +160,7 @@ def test_hidden_terminal() -> None:
     """
     Tests hidden terminal behavior, AMPLIFY 10 TIMES
     """
-    arena = Arena("./test_mesh/test_arenas/hidden-terminal.json")
+    arena = Arena("./test_cope/test_arenas/hidden-terminal.json")
     node_mapping = arena.get_nodes()
     for i in range(1, 4):
         n = 'n' + str(i)
@@ -169,24 +181,4 @@ def test_hidden_terminal() -> None:
                ) == 0, 'expected length of n3\'s queue to be 0'
     n2 = node_mapping['n2']
     assert len(n2.get_queue_state()) == 0, 'n2 should not have gotten a packet'
-    return
-
-
-def test_packet() -> None:
-    """
-    Tests that packet objects and helper methods work.
-    """
-    packet_id = 300
-    path = [str(i) for i in range(100)]
-    packet = Packet(256, packet_id, True, path)
-    assert packet.get_path() == path, "packet path is wrong"
-    assert packet.get_is_request() == True, "packet should be a request"
-    assert packet.get_id() == packet_id, "Packet_id is wrong"
-    return
-
-
-def test_links() -> None:
-    """
-    Tests some link probabilities based on how far they are.  
-    """
     return
